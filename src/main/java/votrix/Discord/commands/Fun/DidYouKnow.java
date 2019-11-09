ae3daeb0f94fc0f318715da199d0bd7499c2913e
Avalnn <beearrable@gmail.com>
2019-11-09 12:22:32 +0000

    Added more facts to DidYouKnow & Added to help list

--- src/main/java/votrix/Discord/commands/Fun/DidYouKnow.java
+++ src/main/java/votrix/Discord/commands/Fun/DidYouKnow.java
@@ -18,7 +18,7 @@ public class DidYouKnow extends ListenerAdapter {
         Facts facts = new Facts();
         EmbedBuilder eb = new EmbedBuilder();
 
-        if(args[0].equalsIgnoreCase(data.getPrefix() + "didyouknow")){
+        if (args[0].equalsIgnoreCase(data.getPrefix() + "didyouknow")) {
             if (args.length < 2) {
                 eb.setDescription("You didn't specify enough arguments");
                 eb.setColor(0xff5555);
@@ -44,8 +44,126 @@ public class DidYouKnow extends ListenerAdapter {
                         event.getMessage().delete().queueAfter(15, TimeUnit.SECONDS);
                         eb.clear();
                     });
+                } else if (args.length < 3) {
+                    if (args[1].equalsIgnoreCase("food")) {
+                        Random random = new Random();
+                        int food = random.nextInt(facts.getFood().length);
+                        eb.setDescription("`Did you know " + facts.getAnimals()[food] + "`");
+                        eb.setColor(0xff5555);
+                        eb.setFooter("Votrix Did You Know", data.getSelfAvatar(event));
+                        eb.setTimestamp(Instant.now());
+
+                        event.getChannel().sendMessage(eb.build()).queue((message) -> {
+                            message.delete().queueAfter(15, TimeUnit.SECONDS);
+                            event.getMessage().delete().queueAfter(15, TimeUnit.SECONDS);
+                            eb.clear();
+                        });
+                    }
+                } else if (args.length < 3) {
+                    if (args[1].equalsIgnoreCase("space")) {
+                        Random random = new Random();
+                        int space = random.nextInt(facts.getAnimals().length);
+                        eb.setDescription("`Did you know " + facts.getSpace()[space] + "`");
+                        eb.setColor(0xff5555);
+                        eb.setFooter("Votrix Did You Know", data.getSelfAvatar(event));
+                        eb.setTimestamp(Instant.now());
+
+                        event.getChannel().sendMessage(eb.build()).queue((message) -> {
+                            message.delete().queueAfter(15, TimeUnit.SECONDS);
+                            event.getMessage().delete().queueAfter(15, TimeUnit.SECONDS);
+                            eb.clear();
+                        });
+                    }
+                } else if (args.length < 3) {
+                    if (args[1].equalsIgnoreCase("countries")) {
+                        Random random = new Random();
+                        int countries = random.nextInt(facts.getCountries().length);
+                        eb.setDescription("`Did you know " + facts.getCountries()[countries] + "`");
+                        eb.setColor(0xff5555);
+                        eb.setFooter("Votrix Did You Know", data.getSelfAvatar(event));
+                        eb.setTimestamp(Instant.now());
+
+                        event.getChannel().sendMessage(eb.build()).queue((message) -> {
+                            message.delete().queueAfter(15, TimeUnit.SECONDS);
+                            event.getMessage().delete().queueAfter(15, TimeUnit.SECONDS);
+                            eb.clear();
+                        });
+                    }
+                } else if (args.length < 3) {
+                    if (args[1].equalsIgnoreCase("history")) {
+                        Random random = new Random();
+                        int history = random.nextInt(facts.getHistory().length);
+                        eb.setDescription("`Did you know " + facts.getHistory()[history] + "`");
+                        eb.setColor(0xff5555);
+                        eb.setFooter("Votrix Did You Know", data.getSelfAvatar(event));
+                        eb.setTimestamp(Instant.now());
+
+                        event.getChannel().sendMessage(eb.build()).queue((message) -> {
+                            message.delete().queueAfter(15, TimeUnit.SECONDS);
+                            event.getMessage().delete().queueAfter(15, TimeUnit.SECONDS);
+                            eb.clear();
+                        });
+                    }
+                } else if (args.length < 3) {
+                    if (args[1].equalsIgnoreCase("movies")) {
+                        Random random = new Random();
+                        int movies = random.nextInt(facts.getMovies().length);
+                        eb.setDescription("`Did you know " + facts.getMovies()[movies] + "`");
+                        eb.setColor(0xff5555);
+                        eb.setFooter("Votrix Did You Know", data.getSelfAvatar(event));
+                        eb.setTimestamp(Instant.now());
+
+                        event.getChannel().sendMessage(eb.build()).queue((message) -> {
+                            message.delete().queueAfter(15, TimeUnit.SECONDS);
+                            event.getMessage().delete().queueAfter(15, TimeUnit.SECONDS);
+                            eb.clear();
+                        });
+                    }
+                } else if (args.length < 3) {
+                    if (args[1].equalsIgnoreCase("sports")) {
+                        Random random = new Random();
+                        int sports = random.nextInt(facts.getSports().length);
+                        eb.setDescription("`Did you know " + facts.getSports()[sports] + "`");
+                        eb.setColor(0xff5555);
+                        eb.setFooter("Votrix Did You Know", data.getSelfAvatar(event));
+                        eb.setTimestamp(Instant.now());
+
+                        event.getChannel().sendMessage(eb.build()).queue((message) -> {
+                            message.delete().queueAfter(15, TimeUnit.SECONDS);
+                            event.getMessage().delete().queueAfter(15, TimeUnit.SECONDS);
+                            eb.clear();
+                        });
+                    }
                 }
             }
         }
+    }
+
+    public String getName() {
+        return "Did You Know";
+    }
+
+    public String getDescription() {
+        return "Random facts about the selected categories.";
+    }
+
+    public String getShortDescription() {
+        return "Something you might not know";
+    }
+
+    public String getCategory() {
+        return "Animals, Food, Space, Countries, History, Movies & Sports";
+    }
+
+    public String getRequiredRoles() {
+        return "Everyone";
+    }
+
+    public String getCommandSyntax() {
+        return "```\n" + Data.getPrefix() + "didyouknow {category}\n```";
+    }
+
+    public boolean isDisabled() {
+        return false;
     }
 }
--- src/main/java/votrix/Discord/utils/Facts.java
+++ src/main/java/votrix/Discord/utils/Facts.java
@@ -2,108 +2,179 @@ package votrix.Discord.utils;
 
 public class Facts {
     public String[] getAnimals() {
-    String[] animals = {    
-    "a bear has 42 teeth",
-    "an ostrich's eye is bigger than its brain",
-    "rabbits like licorice",
-    "a lobsters blood is colorless but when exposed to oxygen it turns blue",
-    "armadillos have 4 babies at a time and are all the same sex",
-    "reindeer like bananas",    
-    "Isaac Newton invented the cat door",
-    "more people die from falling coconuts then from shark attacks",
-    "gorillas sleep 14 hours a day",
-    "the lifespan of a squirrel is 9 years",
-    "the stomach acids found in a snakes stomach can digest bones and teeth but not fur or hair",
-    "a giraffe can go longer without water than a camel",
-    "there are more chickens than people",
-    "for every human there are 200 million insects",
-    "the average bed contains over 6 billion dust mites",
-    "an iguana can stay under water for 28 minutes",
-    "emus can't walk backwards",
-    "the average hen lays 228 eggs a year",
-    "more people are allergic to cows milk than any other food",
-    "more people are killed from bees than snakes",
-    "the life span of a house fly is between 10 to 25 days",
-    "a dolphins top speed is 60kmh (37mph)",
-    "the smallest dog is the Chihuahua",
-    "a sharks top speed is 70kmh (44mph)",
-    "there are 70 million sheep in New Zealand (with 4 million people)",
-    "the worlds smallest bird is the 'bee hummingbird' found in Cuba",
-    "the longest recorded flight of a chicken was 13 seconds",
-    "a woodpeckers tongue can wrap around its head twice",
-    "snakes can't blink",
-    "you can tell the sex of a horse by its teeth (most males have 40, females 36)",
-    "sharks never stop moving even when they sleep or rest",
-    "greyhounds can see better than any other dog breed",
-    "most cows produce more milk when they listen to music",
-    "elephants communicate in sound waves below the frequency that humans can hear",
-    "giant tortoises can live in captivity longer than any other animal",
-    "snakes can't bite in rivers or swamps (they would drown otherwise)",
-    "a blue whale can go up to 6 months without eating",
-    "pelicans consume around a 1/3 of its body weight in a single meal",
-    "a cat has 32 muscles in each ear",
-    "goldfish can see both infrared and ultraviolet light",
-    "cats spend 66% of their life asleep",
-    "macadamia nuts are toxic to dogs",
-    "spiders are arachnids and not insects",
-    "Koalas sleep around 18 hours a day",
-    "all insects have 6 legs",
-    "African Grey Parrots have vocabularies of over 200 words",
-    "a giraffe can clean its ears with its 21 inch tongue",
-    "cats have over 100 vocal chords",
-    "camel's milk doesn't curdle",
-    "squirrels can climb trees faster than they can run on the ground",
-    };
-    return animals;
-}
+        String[] animals = { "a bear has 42 teeth", "an ostrich's eye is bigger than its brain",
+                "rabbits like licorice", "a lobsters blood is colorless but when exposed to oxygen it turns blue",
+                "armadillos have 4 babies at a time and are all the same sex", "reindeer like bananas",
+                "Isaac Newton invented the cat door", "more people die from falling coconuts then from shark attacks",
+                "gorillas sleep 14 hours a day", "the lifespan of a squirrel is 9 years",
+                "the stomach acids found in a snakes stomach can digest bones and teeth but not fur or hair",
+                "a giraffe can go longer without water than a camel", "there are more chickens than people",
+                "for every human there are 200 million insects", "the average bed contains over 6 billion dust mites",
+                "an iguana can stay under water for 28 minutes", "emus can't walk backwards",
+                "the average hen lays 228 eggs a year", "more people are allergic to cows milk than any other food",
+                "more people are killed from bees than snakes", "the life span of a house fly is between 10 to 25 days",
+                "a dolphins top speed is 60kmh (37mph)", "the smallest dog is the Chihuahua",
+                "a sharks top speed is 70kmh (44mph)",
+                "there are 70 million sheep in New Zealand (with 4 million people)",
+                "the worlds smallest bird is the 'bee hummingbird' found in Cuba",
+                "the longest recorded flight of a chicken was 13 seconds",
+                "a woodpeckers tongue can wrap around its head twice", "snakes can't blink",
+                "you can tell the sex of a horse by its teeth (most males have 40, females 36)",
+                "sharks never stop moving even when they sleep or rest",
+                "greyhounds can see better than any other dog breed",
+                "most cows produce more milk when they listen to music",
+                "elephants communicate in sound waves below the frequency that humans can hear",
+                "giant tortoises can live in captivity longer than any other animal",
+                "snakes can't bite in rivers or swamps (they would drown otherwise)",
+                "a blue whale can go up to 6 months without eating",
+                "pelicans consume around a 1/3 of its body weight in a single meal", "a cat has 32 muscles in each ear",
+                "goldfish can see both infrared and ultraviolet light", "cats spend 66% of their life asleep",
+                "macadamia nuts are toxic to dogs", "spiders are arachnids and not insects",
+                "Koalas sleep around 18 hours a day", "all insects have 6 legs",
+                "African Grey Parrots have vocabularies of over 200 words",
+                "a giraffe can clean its ears with its 21 inch tongue", "cats have over 100 vocal chords",
+                "camel's milk doesn't curdle", "squirrels can climb trees faster than they can run on the ground" };
+        return animals;
+    }
 
     public String[] getFood() {
-    String[] food = {
-        ""
-    };
-    return food;
-}
-
-    public String[] getGames() {
-    String[] games = {
-        ""
-    };
-    return games;
-}
+        String[] food = { "cabbage is 91% water", "the most valuable nutrients of a potato are its skin",
+                "wheat is the world's most widely grown plant", "Cheerios cereal was originally called Cheerioats",
+                "the grapefruit gets its name from the way it grows in clusters like grapes on a vine",
+                "a pumpkin really is a fruit", "fresh apples float because they contain 25% air",
+                "there is no butter in buttermilk", "almonds are members of the peach family",
+                "an egg contains every vitamin except vitamin C", "Americans eat 35,000 tons of pasta a year",
+                "Americas top selling ice cream flavour is vanilla", "the oldest known vegetable is the pea",
+                "the average hen lays 228 eggs a year", "carrots contain 0% fat",
+                "1 billion snails are served in restaurants each year",
+                "after bananas a zoo gorilla's favorite food is celery",
+                "over 550 hot dogs are eaten every second in the United States",
+                "instant mashed potatoes (dehydrated potato flakes) were invented in Canadian in 1962",
+                "in American supermarkets peanut butter is one of the most frequently purchased items",
+                "ostrich meat is the leanest of all red meats",
+                "banana plants can grow as high as 6 meters (20 feet) tall",
+                "blueberries are very high in antioxidants", "that in developed countries 27% of food is thrown away",
+                "a banana contains 75% water", "cucumbers are 96% water.", "bananas grow pointing upwards.",
+                "the largest exporter of sugar is Cuba", "turnips turn green when sunburnt",
+                "lettuce is the worlds most popular green vegetable",
+                "the average Americans diet consists of 55% junk food", "the most eaten fruit in America is the banana",
+                "the ancient Greeks first grew carrots as a form of medicine and not a food",
+                "over a third of all pineapples come from Hawaii",
+                "the frozen foods were first introduced in the 1920.",
+                "the bigger the orange the sweeter sweeter it is", "the world's most expensive spice is saffron",
+                "after you've eaten it takes approximately 12 hours for food to entirely digest",
+                "$30 of raw popcorn translates into $3,000 in sales at movie theaters",
+                "the fortune cookie was invented in San Francisco",
+                "the first Burger King was opened in Florida Miami in 1954", "the croissant was invented in Austria",
+                "In eastern Africa you can buy beer brewed from bananas", "popcorn was invented by the Aztec Indians",
+                "french fries are originally from Belgium",
+                "apples are more effective at waking you up in the morning than coffee",
+                "grapes explode when you put them in the microwave",
+                "rice is the staple food for 50% of the worlds population",
+                "there are around 30 milligrams of caffeine in the average chocolate bar while a cup of coffee has around 100 to 150" };
+        return food;
+    }
 
     public String[] getSpace() {
-    String[] space = {
-        ""
-    };
-    return space;
-}
+        String[] space = { "there is a volcano on Mars three times the size of Everest",
+                "the Sun’s mass takes up 99.86% of the solar system", "the Moon was once a piece of the Earth",
+                "the largest known asteroid is 965 km (600 mi) wide",
+                "if two pieces of the same type of metal touch in space they will permanently bond",
+                "in 3.75 billion years the Milky Way and Andromeda galaxies will collide",
+                "one day on Venus is longer than one year", "neutron stars can spin 600 times per second",
+                "halley's Comet won’t orbit past Earth again until 2061",
+                "the hottest planet in our solar system is 450° C", "the sun is 330,330 times larger than the Earth",
+                "from space the brightest man made place is Las Vegas",
+                "due to gravitational effects you weigh is slightly less when the moon is directly overhead",
+                "if you could drive to the sun at a speed of 88.5 km/h (55 mp/h) it would take around 193 years",
+                "hydrogen is the most abundant element in the Universe (75%)", "the moon is 27% the size of the Earth",
+                "the center of the Sun is approximately 15 million C (27 million F)",
+                "an astronaut can be up to 2 inches taller returning from space (the cartilage disks in the spine expand in the absence of gravity)" };
+        return space;
+    }
 
     public String[] getCountries() {
-    String[] countries = {
-        ""
-    };
-    return countries;
-}
+        String[] countries = { "over 200 languages are spoken in Europe", "the largest country in Europe is Russia",
+                "Ethiopia and Liberia are the only two countries in Africa that were not colonized by the European powers",
+                "after Christianity, Judaism is the 2nd largest Religion in the U.S.A",
+                "90% of the world’s fresh water is in Antarctica", "Antarctica is the largest desert in the world",
+                "the smallest country in Europe is Vatican", "Disneyland is the most visited place in Europe",
+                "no pig is allowed to be called Napoleon in France", "Africa has 54 countries",
+                "10 million people visit the Great Wall of China every year",
+                "Nigeria has the highest rate of twin births in the world",
+                "the Marshall Islands in the Pacific Ocean are the most endangered island nation",
+                "Greenland is the country that can’t join FIFA because of bad weather conditions and grass can not grow there",
+                "a third of world’s languages are spoken in Africa", "coffee is grown commercially in Hawaii",
+                "Papua New Guinea is the most linguistically diverse country in the world with 851 individual languages listed",
+                "the oldest state in the world is San Marino", "that there are no polar bears in Antarctica",
+                "Antarctica is the only place in the world that has only one ATM",
+                "the largest island in the world is Green Land",
+                "the Pyramid of Giza is the oldest of the seven wonders of the Ancient world, that is still in existence and it is located in Egypt",
+                "the official animal of Scotland is the unicorn", };
+        return countries;
+    }
 
     public String[] getHistory() {
-    String[] history = {
-        ""
-    };
-    return history;
-}
+        String[] history = { "England's Stonehenge is over 5,000 years old",
+                "the first ground vacuum packed coffee was first introduced in 1900", "Disneyland opened in 1955",
+                "the 1st full length animated film was released by Disney Studios in 1937 (it was Snow White and the seven dwarfs)",
+                "the hamburger was invented in 1900", "Hawaii officially became apart of the US on June 14 1900",
+                "the dollar was established as the official currency of the US in 1785",
+                "in 1900 the average life span in the US was 47", "flushable toilets were in use in ancient Rome",
+                "the first credit card was a Diner's Club card issued in 1950",
+                "the world's first roller coaster opened in 1884 at Coney Island New York",
+                "England's first great industry was wool", "mapping is older than writing",
+                "the corkscrew was invented in 1890", "the original name of Bank of America was Bank of Italy",
+                "the winter of 1932 was so cold that Niagara Falls froze over completely",
+                "the $ sign was introduced in 1788", "the term 'disc jockey' was first used in 1937",
+                "the first train reached a top speed of only 8 kmh (5 mph)", "New York was once called New Amsterdam",
+                "Hawaii officially became apart of the US in 1900", "New York's Central Park was opened in 1876",
+                "the word old English word 'juke' meaning dancing lends its name to the juke box",
+                "the first rugby club was formed in 1843",
+                "the yo-yo was originally used as a weapon for hunting the in the Philippines",
+                "draughts (checkers) is older than chess", "the first metered taxi was introduced in 1907",
+                "the Chinese used fingerprints as a method of identification as far back as AD 700",
+                "the revolving door was invented in 1888",
+                "Sir Isaac Newton was 23 when he discovered the law of gravity",
+                "the drinking straw was invented in 1886", "paper money was first used in China",
+                "Scissors were most likely invented ancient Egypt",
+                "all of the cobble stones used to line the streets in New York were originally stones found in the hulls of Belgian ships ballast",
+                "Diet Coke was introduced in 1982", "Shakespeare invented the words assassination and bump",
+                "that in the 17th century the value of pi was known to 35 decimal places (today to 1.2411 trillion)",
+                "it took Leonardo Da Vinci 10 years to paint Mona Lisa",
+                "the atomic symbol for iron is Fe (the original name for iron was Ferric)",
+                "the first toothbrush was invented in 1498",
+                "before 1850 golf balls were made of leather and were stuffed with feathers",
+                "the Colgate toothpaste company started out making starch, soap, and candles",
+                "most dinosaurs lived to be more than 100 years old" };
+        return history;
+    }
 
     public String[] getMovies() {
-    String[] movies = {
-        ""
-    };
-    return movies;
-}
+        String[] movies = { "there are 22 stars in the Paramount studios logo",
+                "the movie Pulp Fiction cost $8 million to make with $5 million going towards actor's salaries",
+                "the movie 'Wayne's World' was filmed in two weeks",
+                "the film 'Mary Poppins' was filmed entirely indoors",
+                "all of the clocks in the movie Pulp Fiction are fixed to 4:20", "55% of all movies are rated R",
+                "'Babe' was played by over 50 pigs", "the first James Bond movie was called Dr. No",
+                "before beginning his movie career Keanu Reeves managed a pasta shop in Toronto Canada",
+                "the 1st full length animated film was released by Disney Studios in 1937 (it was Snow White and the seven dwarfs)" };
+        return movies;
+    }
 
     public String[] getSports() {
-    String[] sports = {
-        ""
-    };
-    return sports;
-}
-    
+        String[] sports = { "the average golf ball has 336 dimples",
+                "if you filled a matchbox with gold it could be flattened into a sheet the size of a tennis court",
+                "tennis was originally played with bare hands", "the Olympic flag was designed in 1913",
+                "a bowling pin will fall at a tilt of 7.5 degrees",
+                "the average soccer ball is made up of 32 leather panels and held together by 642 stitches",
+                "soccer is the most followed sport", "volleyball was invented in 1895",
+                "the first rugby club was formed in 1843", "the most common injury in ten pin bowling is a sore thumb",
+                "orienteering originated from Sweden",
+                "before 1850 golf balls were made of leather and were stuffed with feathers",
+                "the game of Tug-of-war was an Olympic sport from 1900-1920",
+                "the surface area of your lungs is roughly the same size as a tennis court",
+                "according to manufacturer Spalding the average lifespan of an NBA basketball is 10,000 bounces" };
+        return sports;
+    }
 }
