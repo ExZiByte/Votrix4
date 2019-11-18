95ea2095aababf0bc22b6d417bf69de68e60816c
Avalnn <beearrable@gmail.com>
2019-11-18 18:35:57 +0000

    Added mentioning to Userinfo

--- src/main/java/votrix/Discord/commands/Miscellaneous/Userinfo.java
+++ src/main/java/votrix/Discord/commands/Miscellaneous/Userinfo.java
@@ -13,34 +13,36 @@ public class Userinfo extends ListenerAdapter {
         Data data = new Data();
 
         if (args[0].equalsIgnoreCase(data.getPrefix() + "userinfo")) {
+            if (args.length < 2) {
 
-            StatusChecker statuscheck = new StatusChecker();
-            String status = event.getMember().getOnlineStatus().toString();
-            if (status == "ONLINE") {
-                statuscheck.online(event);
-            } else if (status == "IDLE") {
-                statuscheck.away(event);
-            } else if (status == "DO_NOT_DISTURB") {
-                statuscheck.dnd(event);
-            } else if (status == "OFFLINE") {
-                statuscheck.invisible(event);
-            } else {
-                statuscheck.nullstatus(event);
-            }
-        } else if (args.length > 1) {
-            StatusChecker statuscheck = new StatusChecker();
-            Member mentioned = event.getMessage().getMentionedMembers().get(0);
-            String status = mentioned.getOnlineStatus().toString();
-            if (status == "ONLINE") {
-                statuscheck.onlinementioned(event);
-            } else if (status == "IDLE") {
-                statuscheck.awaymentioned(event);
-            } else if (status == "DO_NOT_DISTURB") {
-                statuscheck.dndmentioned(event);
-            } else if (status == "OFFLINE") {
-                statuscheck.invisiblementioned(event);
-            } else {
-                statuscheck.nullstatusmentioned(event);
+                StatusChecker statuscheck = new StatusChecker();
+                String status = event.getMember().getOnlineStatus().toString();
+                if (status == "ONLINE") {
+                    statuscheck.online(event);
+                } else if (status == "IDLE") {
+                    statuscheck.away(event);
+                } else if (status == "DO_NOT_DISTURB") {
+                    statuscheck.dnd(event);
+                } else if (status == "OFFLINE") {
+                    statuscheck.invisible(event);
+                } else {
+                    statuscheck.nullstatus(event);
+                }
+            } else if (args.length > 1) {
+                StatusChecker statuscheck = new StatusChecker();
+                Member mentioned = event.getMessage().getMentionedMembers().get(0);
+                String status = mentioned.getOnlineStatus().toString();
+                if (status == "ONLINE") {
+                    statuscheck.onlinementioned(event);
+                } else if (status == "IDLE") {
+                    statuscheck.awaymentioned(event);
+                } else if (status == "DO_NOT_DISTURB") {
+                    statuscheck.dndmentioned(event);
+                } else if (status == "OFFLINE") {
+                    statuscheck.invisiblementioned(event);
+                } else {
+                    statuscheck.nullstatusmentioned(event);
+                }
             }
         }
     }
